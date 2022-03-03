efa8b0aa9afe7072d5f398b9fedc7787bdaad976
Joshua Gonzalez <gonjosh@gmail.com>
2022-03-03 01:39:27 -0500

    commit tree 4b8c1005291c521a400c88b983739a9a83edcbae

---
+++ index.js
@@ -0,0 +1,23 @@
+function shout(string) {
+    return string.toUpperCase();
+}
+function whisper(string) {
+    return string.toLowerCase();
+}
+function logShout(string) {
+    console.log('HELLO'.toUpperCase())
+}
+function logWhisper(string) {
+    console.log('Hello'.toLowerCase())
+}
+function sayHiToHeadphonedRoommate(string) {
+    if (string === string.toLowerCase()){
+        return "I can\'t hear you!" } 
+        if (string === string.toUpperCase())
+            return "YES INDEED!"
+            else (string === string.toUpperCase())
+                return "I would love to!"}
+            
+            
+        
+
